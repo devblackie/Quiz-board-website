319f2fd3048de6adaf8c50b6321d4df4841c4b9c
devblackie <newtieblackie@gmail.com>
2022-05-16 09:59:36 +0300

    linked js to the main sheet

--- index.js
+++ index.js
@@ -1,13 +1,44 @@
-const startButton = document.getElementsByClassName()
-function startQuiz(){
-
-}
-
-
-function nextQuestion (){
-
-}
-
-function selectAnswer(){
+function result(){
+    var score=0;
+    if (document.getElementById(correct10%).checked)
+    {
+        score++;
+    }
+    if (document.getElementById(correct20%).checked)
+    {
+        score++;
+    }
+    if (document.getElementById(correct30%).checked)
+    {
+        score++;
+    }
+    if (document.getElementById(correct40%).checked)
+    {
+        score++;
+    }
+    if (document.getElementById(correct50%).checked)
+    {
+        score++;
+    }
+    if (document.getElementById(correct60%).checked)
+    {
+        score++;
+    }
+    if (document.getElementById(correct70%).checked)
+    {
+        score++;
+    }
+    if (document.getElementById(correct80%).checked)
+    {
+        score++;
+    }
+    if (document.getElementById(correct90%).checked)
+    {
+        score++;
+    }
+    if (document.getElementById(correct100%).checked)
+    {
+        score++;
+    }
 
 }
